c415356defe7795b129d01265a3ccfa48a46819d
Jamesni <2667151484@qq.com>
2022-04-09 13:41:10 -0400

    WORKING 5 BALL AUTON

--- src/main/java/frc/robot/Auton/toPosTest2.java
+++ src/main/java/frc/robot/Auton/toPosTest2.java
@@ -11,26 +11,52 @@ import frc.robot.Util.autoPathPoint;
 
 public class toPosTest2 extends SequentialCommandGroup {
     public toPosTest2() {
-        autoPathPoint[] positions1 = new autoPathPoint[]{
-            new autoPathPoint(new Pose2d(0.9,0,Rotation2d.fromDegrees(-90)), true, false),
+        autoPathPoint[] positions1 = new autoPathPoint[] {
+                new autoPathPoint(new Pose2d(0.9, 0, Rotation2d.fromDegrees(-90)), true, false),
         };
 
-        autoPathPoint[] positions2 = new autoPathPoint[]{
-            new autoPathPoint(new Pose2d(0,0,Rotation2d.fromDegrees(-225)), false, false),
-            new autoPathPoint(new Pose2d(0,2,Rotation2d.fromDegrees(-225)), false, true),
-            new autoPathPoint(new Pose2d(0.1,3.7,Rotation2d.fromDegrees(-90)), false, false),
-            new autoPathPoint(new Pose2d(-0.3,4.2,Rotation2d.fromDegrees(-55)), false, true),
-            new autoPathPoint(new Pose2d(-1,3.1,Rotation2d.fromDegrees(-55)), true, true),
+        autoPathPoint[] positions2 = new autoPathPoint[] {
+                new autoPathPoint(new Pose2d(0, 0, Rotation2d.fromDegrees(-225)), false, false),
+                new autoPathPoint(new Pose2d(0, 1.5, Rotation2d.fromDegrees(-225)), false, true, 0.1,
+                        Constants.thetaToPosTolerance),
+                new autoPathPoint(new Pose2d(0, 2.4, Rotation2d.fromDegrees(-180)), false, true, 0.1,
+                        Constants.thetaToPosTolerance),
+                new autoPathPoint(new Pose2d(0.1, 4.1, Rotation2d.fromDegrees(-90)), false, true, 0.1,
+                        Constants.thetaToPosTolerance),
+                new autoPathPoint(new Pose2d(-1.3, 3.7, Rotation2d.fromDegrees(-35)), false, true, 0.1,
+                        Constants.thetaToPosTolerance),
+                new autoPathPoint(new Pose2d(-1.4, 3.7, Rotation2d.fromDegrees(-55)), true, true),
         };
 
+        autoPathPoint[] positions3 = new autoPathPoint[] {
+                new autoPathPoint(new Pose2d(0.9, 3.35, Rotation2d.fromDegrees(-215)), false, true),
+        };
+
+        autoPathPoint[] positions4 = new autoPathPoint[] {
+                new autoPathPoint(new Pose2d(-0.9, -2.55, Rotation2d.fromDegrees(-35)), false, true),
+                new autoPathPoint(new Pose2d(-0.9, -3.55, Rotation2d.fromDegrees(-35)), true, true),
+        };
+        System.out.println("Hello!");
         addCommands(
-            new intakeDown(),
-            new setShooterRPM(7000),
-            new toArray(positions1),
-            new shoot(1, 7000),
-            new setShooterRPM(7000),
-            new toArray(positions2),
-            new shoot(2, 7000)
+                new ParallelRaceGroup(
+                        new SequentialCommandGroup(
+                                new intakeDown(),
+                                new setShooterRPM(7000),
+                                new toArray(positions1),
+                                new shoot(7000),
+                                new setShooterRPM(7000),
+                                new toArray(positions2),
+                                new shoot(7000),
+                                new waitTilBall(),
+                                new shoot(7000),
+                                new toArray(positions3),
+                                new intake(0,0.5),
+                                new toArray(positions4),
+                                new shoot(7000),
+                                new waitTilBall(),
+                                new shoot(7000)),
+                        new timerCommand())
+
         );
 
     }
